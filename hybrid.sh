a5f87ca7c3d6114575c35770f95ce9f48c3a45e4
Diamond Bond <diamond.nigel75@Gmail.com>
2015-03-30 22:39:49 -0700

    Added proprietary functions
    added the following functions with content or placeholders:
    var()
    title()
    body()
    session_behaviour()

--- hybrid.sh
+++ hybrid.sh
@@ -1,1 +1,25 @@
 #hybrid.sh by Pizza_Dox
+
+var(){
+	#version_control
+	ver_revision=$date
+	ver_date=$date
+	#debug control
+	userdebug=1
+}
+
+title(){
+	#temp
+}
+
+body(){
+	#temp
+}
+
+session_behaviour(){
+	#call functions
+	var
+	if [ userdebug == 1 ]; then
+		echo "debugging enabled"
+	fi
+}
