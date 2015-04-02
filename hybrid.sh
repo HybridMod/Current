5038f3eeb8623c4a1459bd785cf4ef5003e1d36f
Diamond Bond <diamond.nigel75@Gmail.com>
2015-04-02 06:56:57 -0700

    Merged simple color control
    Allows for use of color control around text output, will add more colors
    soon.
    Took forever since my hand is messed up and i can only type with my
    leftie...

--- hybrid.sh
+++ hybrid.sh
@@ -7,6 +7,13 @@ var(){
 	userdebug=1
 	#misc control
 	DATE=`date +%d-%m-%Y`
+	#color control
+	red='\033[0;31m'
+	green='\033[0;32m'
+	yellow='\033[0;33m'
+	cyan='\033[0;36m'
+	white='\033[0;97m'
+	nc='\033[0m' # No Color
 }
 
 title(){
@@ -37,16 +44,16 @@ clear
 var
 #run conditional statements
 if [ $userdebug == 1 ]; then
-	echo -e "\e[1;32mDebug information:\e[0m" #green
+	echo -e "${green}Debug information:${nc}" #green
 	#echo -e "\e[1;31mDebug information:\e[0m" #red
 	echo ""
-	echo "SYSTEM"
+	echo "${yellow}SYSTEM${nc}"
 	echo "Vendor: $( getprop ro.product.brand )"
 	echo "Model: $( getprop ro.product.model )"
 	echo "ROM: $( getprop ro.build.display.id )"
 	echo "Android Version: $( getprop ro.build.version.release )"
 	echo ""
-	echo "SCRIPT"
+	echo "${yellow}SCRIPT${nc}"
 	echo "Hybrid Version: $DATE"
 	echo ""
 	sleep 5;
