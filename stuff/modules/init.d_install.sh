194630dd138f4b4df8b4a1d9763eace9c4d858ca
Diamond Bond <diamond.nigel75@Gmail.com>
2015-04-05 12:15:44 -0700

    added init.d_install.sh module
    template for on-boot action.

---
+++ stuff/modules/init.d_install.sh
@@ -0,0 +1,22 @@
+if [ $usagetype == 0 ]; then
+  #temp
+fi
+
+if [ $usagetype == 1 ]; then
+  if [ $initd == 1 ]; then
+    $sysrw
+    mkdir -p /system/etc/init.d
+    touch /system/etc/init.d/#temp
+    chmod 755 /system/etc/init.d/#temp
+    echo -ne "" > /system/etc/init.d/#temp
+cat >> /system/etc/init.d/#temp <<EOF
+#!/system/bin/sh
+sleep #temp;
+
+#temp
+
+EOF
+  fi
+fi
+
+$sysro
