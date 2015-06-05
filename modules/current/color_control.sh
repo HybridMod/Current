4c349fc8af6526fc44a6a71b51591e25221bcb44
Diamond Bond <diamond.nigel75@Gmail.com>
2015-06-05 21:18:32 +0530

    color_control.sh introduced imported from hybrid.sh
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

---
+++ modules/current/color_control.sh
@@ -0,0 +1,14 @@
+#color control
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+cyan='\033[0;36m'
+white='\033[0;97m'
+
+#format control
+bld='\033[0;1m' #bold
+blnk='\033[0;5m' #blinking
+nc='\033[0m' #no color
+
+#eg:
+echo "${cyan}Sample Text${nc}"
