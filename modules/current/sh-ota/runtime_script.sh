d4bea3f264ef5a89b9695ebc90d1730c5644cc9a
Diamond Bond <diamond.nigel75@Gmail.com>
2015-06-05 21:15:12 +0530

    renamed and commented renamed to example.sh and commented with dev-friendly comments for easy merging
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- modules/current/sh-ota/runtime_script.sh
+++
@@ -1,6 +0,0 @@
-# This goes to the top of your script / check_update function
-am start -a android.intent.action.VIEW -n com.android.browser/.BrowserActivity https://www.yoursite.com/check.update #URL to download your check.update file
-sleep 3 #not necessary
-am force-stop com.android.browser
-sleep 2 #not necessary
-sh $EXTERNAL_STORAGE/Download/check.update #legacy bash does not support the $EXTERNAL_STORAGE variable
