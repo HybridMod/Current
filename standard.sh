f3c7f6b31629f94105f3830da1161a00db8d8bba
yop yop yop <hoholee12@naver.com>
2015-09-07 20:01:42 +0900

    API update #11
    -added titlemate

--- standard.sh
+++ standard.sh
@@ -362,6 +362,40 @@ part_line(){
 	done
 	echo $@
 }
+titlemate(){
+	title=$@
+	if [ ! "$title" ]; then
+		title=$(checkers)
+	fi
+	echo -n -e '\e[3m'
+	if [ "$(($(print_RANDOM_BYTE)%2))" == 0 ]; then
+		lines=$(stty size | awk '{print $1}')
+		skip=$((lines/10))
+		sequence=10
+		if [ "$skip" == 0 ]; then
+			skip=1
+			sequence=$lines
+		fi
+		for i in $(seq 1 $sequence); do
+			echo -n -e "\e[2J\e[$((lines-i*skip));1H$title"
+			sleep 0.1
+		done
+	else
+		lines=$(stty size | awk '{print $2}')
+		skip=$((lines/10))
+		sequence=10
+		if [ "$skip" == 0 ]; then
+			skip=1
+			sequence=$lines
+		fi
+		for i in $(seq 1 $sequence); do
+			echo -n -e "\e[2J\e[1;$((lines-i*skip))H$title"
+			sleep 0.1
+		done
+	fi
+	echo -n -e "\e[2J\e[0;0H$title"
+	echo -e "\e[0m"
+}
 error(){
 	message=$@
 	if [ "$(echo $message | grep \")" ]; then
